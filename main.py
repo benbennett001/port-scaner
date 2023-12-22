946d01d6c20bde81d968beb6a74c6119091c4ad2
benbennett001 <usejf96@gmail.com>
2023-12-22 11:43:09 -0500

    change the text doc to a input var & some formating

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+
 import socket
 
+# ip validation 
 def is_valid_ip(ip_str: str) -> bool:
     ip_sub_list = ip_str.split(sep='.')
 
@@ -12,49 +14,64 @@ def is_valid_ip(ip_str: str) -> bool:
 
     return True
 
+# scan for one ip add
 def sop_one():
     ip_str = input('Enter the IP addr: ')
 
+    # call ip validation
     if is_valid_ip(ip_str) == False:
         return      
     
+    # get ports number
     ports = int(input('Enter a number of ports: '))
 
     scan_port(ip_str, ports)
 
+# scan function using socket library
 def scan_port(ip_str, ports):
-    print(f'\tStart scanning for {ip_str}')
+    print(f'\tStart scanning for >> {ip_str}  <<')
+
     for port in range(1, ports):
         try:
+            # af_inet and sock_stream are the simplest functions
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(1)
+            # host and port pair >> touple 
             s.connect((ip_str, port))
             print(f'The port {port} is open')
 
+        # TODO: 
+        # add the no route to host error code for not assing ips
         except (TimeoutError, ConnectionRefusedError):
             pass
         
         finally:
             s.close()
 
+# scan for a list of ips
 def sop_group():
+    file_name = input('Name of the list of ips to scan: ')
     clean_list = []
-    with open('devices.txt', 'r') as devices_text:
+    with open(file_name, 'r') as devices_text:
         ip_list = devices_text.readlines()
 
     for ip in ip_list:
+        # cleaning the list
         ip = ip.removesuffix('\n')
         clean_list.append(ip)
         
+    #goin through the list
     for ip in clean_list:
+
         if is_valid_ip(ip) is False: 
             print(f'invalid ip: {ip}\n' ) 
             continue
 
-        print(f'For ip {ip} -->')
-        ports = int(input('Enter a number of ports: '))
+        print(f'#\tFor ip --> {ip}:')
+        ports = int(input('#\tEnter a number of ports: '))
         scan_port(ip, ports)
 
+# display the file with the list of ips 
 def print_file():
     try:
         file_name = input('\tName of the file you want to read: ')
@@ -63,10 +80,10 @@ def print_file():
             print('The file content is:')
             print(text)
     except FileNotFoundError:
-        print('Failed to open the file devices.txt.\n')
+        print('### Failed to open the file devices.txt. ###\n')
         exit()
 
-
+# MAIN MENU
 while True:
     print('\nPort Scanner Menu:')
     print('\t1- Print the file contents.')
